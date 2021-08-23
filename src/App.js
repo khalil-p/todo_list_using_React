8ab0b29f1bccd9bff1578ba455a03da9be95ab81
khalil-p <pkhalilmk@gmail.com>
2021-08-23 21:28:12 +0530

    Chat Increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import Resturant from "./component/Basics/Resturant";
-
+// import Resturant from "./component/Basics/Resturant";
+// import UseState from "./component/Hooks/UseState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
 const App = () => {
-  return <Resturant />;
+  return <UseReducer />;
 };
 
 export default App;
---
+++ src/component/Hooks/useReducer.js
@@ -0,0 +1,46 @@
+import React, { useState, useReducer, useEffect } from "react";
+import "./style.css";
+
+const reducer = (state, action) => {
+  if (action.type === "INCR") {
+    state = state + 1;
+  }
+  if (state > 0 && action.type === "DECR") {
+    state = state - 1;
+  }
+
+  return state;
+};
+
+const UseReducer = () => {
+  const initialData = 0;
+  //   const [myNum, setMyNum] = useState(initialData);
+  const [state, dispatch] = useReducer(reducer, initialData);
+  useEffect(() => {
+    document.title = `Chats(${state})`;
+  });
+
+  return (
+    <>
+      <div className="center_div">
+        <p>{state}</p>
+        <div class="button2" onClick={() => dispatch({ type: "INCR" })}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          INCR
+        </div>
+        <div class="button2" onClick={() => dispatch({ type: "DECR" })}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          DECR
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UseReducer;
